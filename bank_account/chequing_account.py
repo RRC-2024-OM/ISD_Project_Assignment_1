0a58dc5294fc9fab16493ea0418703f195445839
OM J PATEL <opatel@academic.rrc.ca>
2024-10-03 17:11:30 -0500

    string representation and get calculated service charges for chequing account completed.

--- bank_account/chequing_account.py
+++ bank_account/chequing_account.py
@@ -22,7 +22,9 @@ class ChequingAccount(BankAccount):
             account_number (int): The account number for the bank account.
             client_number (int): The client number associated with the bank account.
             balance (float): The initial balance of the bank account.
-            date_created (date): The date the account was created."""
+            date_created (date): The date the account was created.
+            overdraft_limit (float): The overdraft limit for the chequing account.
+            overdraft_rate (float): The overdraft rates."""
         
         super().__init__(account_number, client_number, balance, date_created)
 
@@ -35,5 +37,32 @@ class ChequingAccount(BankAccount):
             self.__overdraft_rate = float(overdraft_rate)
         except ValueError:
             self.__overdraft_rate = 0.05
+
+    def __str__(self) -> str:
+        """Gives a string that is representation of Chequing account.
+
+        Returns:
+            A string representation of account_number, client_number, balance, overdraft_limit and overdraft_rate."""
+        
+        return (f"Account Number: {self.account_number} "
+                f"Client Number: {self.client_number} "
+                f"Balance: ${self.balance:,.2f}\n"
+                f"Overdraft Limit: ${self.__overdraft_limit:,.2f} "
+                f"Overdraft Rates: {self.__overdraft_rate * 100:.2f}% "
+                f"Account Type: Chequing\n")
+    
+    def get_service_charges(self) -> float:
+        """Return Calculated service charges for chequing account.
+
+        Returns:
+            float: The service charges for the chequing account."""
+        
+        overdraft_interest = (self.__overdraft_limit - self.balance) * self.__overdraft_rate
+
+        if self.balance >= self.__overdraft_limit:
+            return self.BASE_SERVICE_CHARGE
+        else:
+            return self.BASE_SERVICE_CHARGE + overdraft_interest
+
 
 
