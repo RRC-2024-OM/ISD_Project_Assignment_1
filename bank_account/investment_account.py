b911d8c6fd57d4e3c1bdee259e566fcb656a371e
OM J PATEL <opatel@academic.rrc.ca>
2024-10-03 18:22:40 -0500

    Invoked the init method, set management fee as given and added validation.

--- bank_account/investment_account.py
+++ bank_account/investment_account.py
@@ -15,4 +15,21 @@ class InvestmentAccount(BankAccount):
         management_fee (float) : Fees for managing the investment account."""
     
     TEN_YEARS_AGO = date.today() - timedelta(days=10 * 365.25)
-    
+
+    def __init__(self, account_number: int, client_number: int, balance: float, date_created: date, management_fee: float):
+        """New InvestmentAccount class created.
+
+        Arguments:
+            account_number (int): The account number for the bank account.
+            client_number (int): The client number associated with the bank account.
+            balance (float): The initial balance of the bank account.
+            date_created (date): The date account was created.
+            management_fee (float): The fee for the investment account management."""
+        
+        super().__init__(account_number, client_number, balance, date_created)
+
+        try:
+            self.__management_fee = float(management_fee)
+        except ValueError:
+            self.__management_fee = 2.55
+
