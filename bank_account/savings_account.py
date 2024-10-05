5c2a6778c5732008e87ad233dcda56490f585596
OM J PATEL <opatel@academic.rrc.ca>
2024-10-04 19:59:24 -0500

    Add SavingsAccount class with attributes and methods for savings accounts.

---
+++ bank_account/savings_account.py
@@ -0,0 +1,62 @@
+"""Author: Om Patel
+Date: 2024-10-04
+
+Description : This module defines a SavingsAccount class that is sub class of BankAccount class.
+It has attributes and required methods for the Saving Account.
+"""
+from datetime import date, timedelta
+from bank_account.bank_account import BankAccount
+
+class SavingsAccount(BankAccount):
+    """The class represent Savings Account inherits for BankAccount which is superclass abstract.
+
+    Attributes:
+    SERVICE_CHARGE_PREMIUM (float): Savings Account premium service charges.
+
+    minimum_balance (float): Savings account minimum balance.
+    """
+
+    SERVICE_CHARGE_PREMIUM: float = 2.00
+
+    def __init__(self, account_number: int, client_number: int, balance: float, date_created: date, minimum_balance: float):
+        """New SavingsAccount class created.
+
+        Arguments:
+            account_number (int): The account number for the bank account.
+            client_number (int): The client number associated with the bank account.
+            balance (float): The initial balance of the bank account.
+            date_created (date): The date account was created.
+            minimum_balance (float): Savings account 
+        """
+        super().__init__(account_number, client_number, balance, date_created)
+
+        try:
+            self.__minimum_balance = float(minimum_balance)
+        except ValueError:
+            self.__minimum_balance = 50.0
+
+    def __str__(self) -> str:
+        """String for Savings Account.
+
+        Returns:
+            Str: A string representing account_number, client_number, balance, date_created, and minimum_balance.
+        """
+
+        return (f"Account Number: {self.account_number} "
+                f"Client Number: {self.client_number} "
+                f"Balance: {self.balance:,.2f}\n"
+                f"Minimum Balance: ${self.__minimum_balance:,.2f} "
+                f"Account Type: Savings\n")
+    
+    def get_service_charges(self) -> float:
+        """Returns the calculated service charge.
+
+        Returns:
+            float: The service charge for savings account.
+        """
+
+        if self.balance >= self.__minimum_balance:
+            return self.BASE_SERVICE_CHARGE
+        else:
+            return self.BASE_SERVICE_CHARGE * self.SERVICE_CHARGE_PREMIUM
+
