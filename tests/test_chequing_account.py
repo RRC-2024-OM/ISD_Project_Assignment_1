06c99d49c802855179637849aeada5304671b050
OM J PATEL <opatel@academic.rrc.ca>
2024-10-06 11:05:17 -0500

    Date input change to test in real time date going on.

--- tests/test_chequing_account.py
+++ tests/test_chequing_account.py
@@ -2,7 +2,8 @@
 Date : 2024-10-03
 
 Description : Test for the Chequing Account.
-To run : python -m unittest tests/test_chequing_account.py"""
+To run : python -m unittest tests/test_chequing_account.py
+"""
 
 import unittest
 from datetime import date
@@ -11,7 +12,7 @@ from bank_account.chequing_account import ChequingAccount
 class TestChequingAccount(unittest.TestCase):
 
     def test_initialize_with_valid_values(self):
-        account = ChequingAccount(2004, 2904, 1000.0, date(2024, 10, 3), -100.0, 0.05)
+        account = ChequingAccount(2004, 2904, 1000.0, date.today(), -100.0, 0.05)
         self.assertEqual(account.account_number, 2004)
         self.assertEqual(account.client_number, 2904)
         self.assertEqual(account.balance, 1000.0)
@@ -19,11 +20,11 @@ class TestChequingAccount(unittest.TestCase):
         self.assertEqual(account._ChequingAccount__overdraft_rate, 0.05)
 
     def test_initialize_overdraft_limit_value_invalid(self):
-        account = ChequingAccount(2004, 2904, 1000.0, date(2024, 10, 3), "invalid", 0.05)
+        account = ChequingAccount(2004, 2904, 1000.0, date.today(), "invalid", 0.05)
         self.assertEqual(account._ChequingAccount__overdraft_limit, -100.0)
 
     def test_initialize_overdraft_rate_invalid(self):
-        account = ChequingAccount(2004, 2904, 1000.0, date(2024, 10, 3), -100.0, "invalid")
+        account = ChequingAccount(2004, 2904, 1000.0, date.today(), -100.0, "invalid")
         self.assertEqual(account._ChequingAccount__overdraft_rate, 0.05)
 
     def test_initialize_account_with_invalid_creation_date(self):
@@ -31,19 +32,19 @@ class TestChequingAccount(unittest.TestCase):
         self.assertEqual(account._date_created, date.today())
 
     def test_compute_service_charges_when_balance_exceeds_overdraft_limit(self):
-        account = ChequingAccount(2004, 2904, 100.0, date(2024, 10, 3), -100.0, 0.05)
+        account = ChequingAccount(2004, 2904, 100.0, date.today(), -100.0, 0.05)
         self.assertEqual(account.get_service_charges(), 0.50)
 
     def test_compute_service_charges_when_balance_below_limit(self):
-        account = ChequingAccount(2004, 2904, -600.0, date(2024, 10, 3), -100.0, 0.05)
+        account = ChequingAccount(2004, 2904, -600.0, date.today(), -100.0, 0.05)
         self.assertEqual(round(account.get_service_charges(), 2), 25.50)
 
     def test_compute_service_charges_when_balance_equals_limit(self):
-        account = ChequingAccount(2004, 2904, -100.0, date(2024, 10, 3), -100.0, 0.05)
+        account = ChequingAccount(2004, 2904, -100.0, date.today(), -100.0, 0.05)
         self.assertEqual(account.get_service_charges(), 0.50)
 
     def test_account_to_string_method(self):
-        account = ChequingAccount(2004, 2904, 1000.0, date(2024, 10, 3), -100.0, 0.05)
+        account = ChequingAccount(2004, 2904, 1000.0, date.today(), -100.0, 0.05)
         expected_str = ("Account Number: 2004 "
                         "Client Number: 2904 "
                         "Balance: $1,000.00\n"
--- tests/test_investment_account.py
+++ tests/test_investment_account.py
@@ -10,14 +10,14 @@ from bank_account.investment_account import InvestmentAccount
 class TestInvestmentAccount(unittest.TestCase):
 
     def test_initialization_valid_numbers(self):
-        account = InvestmentAccount(2004, 2904, 1000.0, date(2024, 10, 4), 2.55)
+        account = InvestmentAccount(2004, 2904, 1000.0, date.today(), 2.55)
         self.assertEqual(account.account_number, 2004)
         self.assertEqual(account.client_number, 2904)
         self.assertEqual(account.balance, 1000.0)
         self.assertEqual(account._InvestmentAccount__management_fee, 2.55)
 
     def test_initialization_with_invalid_fee(self):
-        account = InvestmentAccount(2004, 2904, 1000.0, date(2024, 10, 4), "Invalid fee" )
+        account = InvestmentAccount(2004, 2904, 1000.0, date.today(), "Invalid fee" )
         self.assertEqual(account._InvestmentAccount__management_fee, 2.55)
 
     def test_service_charge_for_just_over_decade(self):
@@ -25,11 +25,11 @@ class TestInvestmentAccount(unittest.TestCase):
         self.assertEqual(account.get_service_charges(), 0.50)
 
     def test_service_charge_for_a_decade(self):
-        account = InvestmentAccount(2004, 2904, 1000.0, date(2014, 10, 4), 2.55)
+        account = InvestmentAccount(2004, 2904, 1000.0, date.today(), 2.55)
         self.assertEqual(0.50, round(account.get_service_charges(), 2))
 
     def test_service_charge_within_a_decade(self):
-        account = InvestmentAccount(2004, 2904, 1000.0, date(2024, 10, 3), 2.55)
+        account = InvestmentAccount(2004, 2904, 1000.0, date.today(), 2.55)
         self.assertEqual(account.get_service_charges(), 3.05)
 
     def test_string_method_for_more_than_a_decade(self):
