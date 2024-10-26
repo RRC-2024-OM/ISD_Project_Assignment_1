12ac437f4a55a61dca8caf8ac8f8a0e5a3928c74
OM J PATEL <opatel@academic.rrc.ca>
2024-10-25 20:21:40 -0500

    Added the missing import of bank account.

--- patterns/strategy/overdraft_strategy.py
+++ patterns/strategy/overdraft_strategy.py
@@ -3,13 +3,14 @@ Description: Pattern implementation for calculating overdraft fees.
 Author: Om Patel 
 """
 from strategy.service_charge_strategy import ServiceChargeStrategy
+from bank_account.bank_account import BankAccount
 
 class OverdraftStrategy(ServiceChargeStrategy):
     def __init__(self, overdraft_limit: float, overdraft_rate: float):
         self.__overdraft_limit = overdraft_limit
         self.__overdraft_rate = overdraft_rate
 
-    def calculate_service_charges(self, account) -> float:
+    def calculate_service_charges(self, account: BankAccount) -> float:
         if account.balance >= self.__overdraft_limit:
             return self.BASE_SERVICE_CHARGE
         else:
