fd3f31e4333fa9b29724776d33d362cc59ed5fd2
OM J PATEL <opatel@academic.rrc.ca>
2024-10-25 23:39:24 -0500

    Subject class for managing the notification.

---
+++ patterns/observer/subject.py
@@ -0,0 +1,19 @@
+"""
+Description : Subject handling observer and managing the notifications.
+Author : Om Patel
+"""
+from observer.observer import Observer
+
+class Subject:
+    def __init__(self) -> None:
+        self._observer = []
+    
+    def attach(self, observer: Observer) -> None:
+        self._observer.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observer.remove(observer)
+    
+    def notify(self, message: str) -> None:
+        for observer in self._observer:
+            observer.update(message)
