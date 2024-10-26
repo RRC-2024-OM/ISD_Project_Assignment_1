dedf75fef8b160de60f65eaebec8050b302966d9
OM J PATEL <opatel@academic.rrc.ca>
2024-10-25 19:33:55 -0500

    strategy for overdraft fees.

---
+++ patterns/strategy/overdraft_strategy.py
@@ -0,0 +1,17 @@
+"""
+Description: Pattern implementation for calculating overdraft fees.
+Author: Om Patel 
+"""
+from strategy.service_charge_strategy import ServiceChargeStrategy
+
+class OverdraftStrategy(ServiceChargeStrategy):
+    def __init__(self, overdraft_limit: float, overdraft_rate: float):
+        self.__overdraft_limit = overdraft_limit
+        self.__overdraft_rate = overdraft_rate
+
+    def calculate_service_charges(self, account) -> float:
+        if account.balance >= self.__overdraft_limit:
+            return self.BASE_SERVICE_CHARGE
+        else:
+            overdraft_interest = (self.__overdraft_limit - account.balance) * self.__overdraft_rate
+            return self.BASE_SERVICE_CHARGE + overdraft_interest
