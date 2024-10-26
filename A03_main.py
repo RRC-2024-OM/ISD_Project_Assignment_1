8d38cdb6bc99a7bbfa2c34d4d3d753c9f53088d0
OM J PATEL <opatel@academic.rrc.ca>
2024-10-26 16:01:45 -0500

    Main file completed.

--- A03_main.py
+++ A03_main.py
@@ -10,40 +10,32 @@ Date: 2024-10-26
 #     Import date
 #     Import Client
 from bank_account.chequing_account import ChequingAccount
-from bank_account.investment_account import InvestmentAccount
 from bank_account.savings_account import SavingsAccount
 from datetime import date
 from client.client import Client
 
 # 2. Create a Client object with data of your choice.
+client_one = Client(client_number = 8888, first_name = "Jayesh", last_name = "Patel", email_address = "jp@gmail.com")
 
+# 3a. Create a ChequingAccount object with data of your choice, using the client_number
+chequing_account = ChequingAccount(account_number = 1111, client_number = client_one.client_number, balance = 5896.00, date_created = date.today(), overdraft_limit = -500.00, overdraft_rate = 0.05) 
 
-
-
-# 3a. Create a ChequingAccount object with data of your choice, using the client_number 
-# of the client created in step 2.
 # 3b. Create a SavingsAccount object with data of your choice, using the client_number 
-# of the client created in step 2.
-
-
-
-
+savings_account = SavingsAccount(account_number = 2222, client_number = client_one.client_number, balance = 14789.00, date_created = date.today(), minimum_balance = 100.00)
 
 # 4 The ChequingAccount and SavingsAccount objects are 'Subject' objects.
 # The Client object is an 'Observer' object.  
 # 4a.  Attach the Client object (created in step 1) to the ChequingAccount object (created in step 2).
+chequing_account.attach(client_one)
+
 # 4a.  Attach the Client object (created in step 1) to the SavingsAccount object (created in step 2).
-
-
-
-
+savings_account.attach(client_one)
 
 # 5a. Create a second Client object with data of your choice.
+client_two = Client(client_number = 9999, first_name = "Milan", last_name = "Jot", email_address = "milanjot@gmail.com")
+
 # 5b. Create a SavingsAccount object with data of your choice, using the client_number 
-# of the client created in this step.
-
-
-
+savings_account_two = SavingsAccount(account_number = 7777, client_number = client_two.client_number, balance = 9564.00, date_created = date.today(), minimum_balance = 500.00)
 
 # 6. Use the ChequingAccount and SavingsAccount objects created 
 # in steps 3 and 5 above to perform transactions (deposits and withdraws) 
@@ -55,3 +47,50 @@ from client.client import Client
 # ensure the methods are invoked using proper exception handling such 
 # that any exception messages are printed to the console.
 
+# Transactions for chequing Account
+try:
+    chequing_account.deposit(11236.01)  
+except ValueError as e:
+    print(e)
+
+try:
+    chequing_account.withdraw(505.00)
+except ValueError as e:
+    print(e)
+
+try:
+    chequing_account.withdraw(6789.00)  
+except ValueError as e:
+    print(e)
+
+# Transactions for savings_account
+try:
+    savings_account.deposit(8564.00)
+except ValueError as e:
+    print(e)
+
+try:
+    savings_account.withdraw(13789.00)  
+except ValueError as e:
+    print(e)
+
+try:
+    savings_account.deposit(158.00)
+except ValueError as e:
+    print(e)
+
+# Transactions for savings_account2
+try:
+    savings_account_two.deposit(6000.00)
+except ValueError as e:
+    print(e)
+
+try:
+    savings_account_two.withdraw(8500.00)  
+except ValueError as e:
+    print(e)
+
+try:
+    savings_account_two.deposit(500.00)
+except ValueError as e:
+    print(e)
